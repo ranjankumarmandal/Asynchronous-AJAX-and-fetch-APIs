6681fde2445bc63c4c411215aef4c5443dce4880
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-10-27 16:58:35 +0530

    api definition

---
+++ 01_API.js
@@ -0,0 +1,8 @@
+// What is API
+
+// API - Application Programming Interface
+// Definition - Its contract provided by one piece of software to another
+// API can be from web API, from PC, from smart phone, or from others. But APIs from Web tech is most common
+// Structured request & response
+// In our 3rd section, we have just worked on an API that takes a request and responds with jokes 
+// In our 3rd section was perfect example of a web API
