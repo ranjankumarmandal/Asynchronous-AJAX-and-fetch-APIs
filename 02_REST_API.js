58ed10fcaf4a42320bb3a2d572ee73132f0b832d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-10-27 16:59:40 +0530

    rest api definition

---
+++ 02_REST_API.js
@@ -0,0 +1,8 @@
+// waht is REST APIs
+
+// REST - REpresentational State Transfer
+// architecture style for designing networked applications
+// Relies on stateless, client server protocol, almost always HTTP
+// Treats server objects as resources that can be created or destroyed
+// Can be used by virtually any programming language
+// All APIs have their own rules and structure 
