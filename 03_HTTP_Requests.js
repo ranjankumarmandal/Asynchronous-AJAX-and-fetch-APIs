b68979b484eab1182e06f52533c4f9fe6b4d1ab7
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-10-27 17:00:14 +0530

    http request types

---
+++ 03_HTTP_Requests.js
@@ -0,0 +1,11 @@
+// Main Types of HTTP Requests -
+
+// GET - Retrieve data from a specified resource
+// POST - Submit data to be processed to a specified resource
+// PUT - Update a specified resource
+// DELETE - Delete a specified resource
+
+// ------------ others (are not used that much) ------------------------
+// HEAD - its same as GET but it douesn't return a body
+// OPTIONS - Returns the supported HTTP methods
+// PATCH - Update Partial resources
